cf76e0d002f84f1526702b0c69b533cc63856093
Ben Lega <benlegavidya@gmail.com>
2025-05-20 14:18:43 +0200

    Cannot grow plant through walls

--- thatgamejam/parts/plant/plant_stem.gd
+++ thatgamejam/parts/plant/plant_stem.gd
@@ -1,6 +1,8 @@
 @tool
 extends Node2D
 
+signal plant_collided
+
 @export var length: float = 100.0
 @onready var line_2d: Line2D = $Line2D
 @onready var end_point: Marker2D = $EndPoint
@@ -8,3 +10,8 @@ extends Node2D
 func _process(_delta: float) -> void:
 	line_2d.points[1].y = -length
 	end_point.position = line_2d.points[1]
+
+
+func _on_collision_detector_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if length > 50.0:
+		plant_collided.emit()
