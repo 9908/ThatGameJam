fbf1f3184c873277395158d7045d55408ecabea3
Ben Lega <benlegavidya@gmail.com>
2025-06-04 17:13:15 +0200

    Plant platform bounces on impact

--- thatgamejam/parts/plant/plant_block.gd
+++ thatgamejam/parts/plant/plant_block.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 
 @onready var plant_area: Area2D = $PlantArea
 @onready var asset: AnimatedSprite2D = $Asset
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func initiate(new_pos: Vector2, plant, id: int):
 	global_position = new_pos
@@ -10,3 +11,12 @@ func initiate(new_pos: Vector2, plant, id: int):
 	plant_area.block_position = id
 	asset.frame = 0
 	asset.play("grow")
+
+func bounce():
+	animation_player.play("bounce")
+
+
+func _on_plant_area_area_entered(area: Area2D) -> void:
+	if area.owner == Globals.player:
+		if Globals.player.velocity.y > 0:
+			bounce()
