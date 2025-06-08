8647bedd54863feda7a08d90d0ac4361676322e0
Ben Lega <benlegavidya@gmail.com>
2025-06-08 12:01:38 +0200

    letterbox integration + unify font to all UI

--- thatgamejam/parts/letter_box/letter_box.gd
+++ thatgamejam/parts/letter_box/letter_box.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 var player_nearby: bool = false
+@onready var animated_sprite: AnimatedSprite2D = $Visual/AnimatedSprite
 
 
 func _physics_process(_delta):
 	if (Input.is_action_just_pressed("grow") or Input.is_action_just_pressed("cut")) and player_nearby:
 		Globals.pop_up_question.set_active(true)
-		
+		animated_sprite.frame = 0
+		animated_sprite.play("default")
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == Globals.player:
